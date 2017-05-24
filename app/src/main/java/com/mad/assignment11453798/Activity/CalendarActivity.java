5d54d74f0e32652c0d6f9467b17f644acd4bf806
ayetee <alex.thratthabeusy@gmail.com>
2017-05-25 00:26:43 +1000

    added twitter - need testing

--- app/src/main/java/com/mad/assignment11453798/Activity/CalendarActivity.java
+++ app/src/main/java/com/mad/assignment11453798/Activity/CalendarActivity.java
@@ -14,14 +14,25 @@ import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
 import com.google.gson.Gson;
 import com.mad.assignment11453798.Pojo.FacebookEvent;
+import com.mad.assignment11453798.Pojo.TwitterTweet;
 import com.mad.assignment11453798.R;
 import com.orm.SugarRecord;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.services.StatusesService;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
 
 public class CalendarActivity extends MainActivity{
     public static final int DATE_CONTENT_REQUEST = 1;
@@ -44,6 +55,7 @@ public class CalendarActivity extends MainActivity{
             @Override
             public void onClick(View v) {
                 getFacebookEvents();
+//                getTweets();
             }
         });
     }
@@ -108,5 +120,34 @@ public class CalendarActivity extends MainActivity{
                     }
                 }
         ).executeAsync();
+    }
+
+    /**
+     * Twitter REST API call
+     * Stores the users tweets into Sugar db
+     */
+    private void getTweets(){
+        TwitterSession currentSession = Twitter.getSessionManager().getActiveSession();
+        TwitterApiClient twitterApiClient = new TwitterApiClient(currentSession);
+        StatusesService statusesService = twitterApiClient.getStatusesService();
+        String name = currentSession.getUserName();
+        Call<List<Tweet>> tweets = statusesService.userTimeline(null, name, 5, null, null, false, false, false, true);//limited to 5 for testing
+        tweets.enqueue(new Callback<List<Tweet>>() {
+            @Override
+            public void success(Result<List<Tweet>> tweets){
+                List<TwitterTweet> twitterTweets = new ArrayList<>();
+//                TwitterTweet.deleteAll(TwitterTweet.class);
+                List<Tweet> tweetList = tweets.data;
+                for(int i=0;i<tweetList.size();i++){
+                    Tweet tweet = tweetList.get(i);
+                    twitterTweets.add(new TwitterTweet(tweet.text,tweet.createdAt));
+                }
+                SugarRecord.saveInTx(twitterTweets);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+            }
+        });
     }
 }
